fbb00837107e87338ecbcf2ecfedc2c6022475b9
Rafael <rafa.shiga@gmail.com>
2020-07-16 19:09:29 -0300

    feat: add menu bar

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Button from '../Button';
+import {
+  Container,
+  Logo,
+  Topside,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  ExitIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+        <Logo />
+        <MenuButton>
+          <HomeIcon />
+          <span>Página Inicial</span>
+        </MenuButton>
+
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+
+        <MenuButton>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButton>
+
+        <MenuButton className="active">
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </Topside>
+
+      <Botside>
+        <Avatar />
+        <ProfileData>
+          <strong>Rafael</strong>
+          <span>@rafael</span>
+        </ProfileData>
+
+        <ExitIcon />
+      </Botside>
+    </Container>
+  );
+};
+
+export default MenuBar;
